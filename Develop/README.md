36859e5a759dcbc70ae036434b26298c56233c1d
Dchica25 <davidchica25@yahoo.com>
2024-12-13 09:49:22 -0500

    new

--- Develop/README.md
+++ Develop/README.md
@@ -1,27 +1,37 @@
 
-  # Trivia game
-  
-  ## Description
-  Play trivia games on the web
-  
-  ## Installation
-  search url and play
-  
-  ## Usage
-  search website and follow webpage instruction
-  
-  ## License
-  This project is licensed under the [MIT License](https://opensource.org/licenses/MIT).
-  
-  ## Contributing
-  none
+# something
 
-  ## GitHub username
-  Dchica25
+![License Badge](https://img.shields.io/badge/license-MIT-blue)
+
+## Description
+very good
+
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+none
+
+## Usage
+none
+
+## License
+This project is licensed under the MIT License.
+
+## Contributing
+no
+
+## Tests
+none
+
+## Questions
+For any questions, please contact me via:
+- GitHub: [david 2 email.com](https://github.com/david 2 email.com)
+- Email: none
   
-  ## Tests
-  none
-  
-  ## Contact
-  davidchica@none.com
-    
--- Develop/index.js
+++ Develop/index.js
@@ -2,102 +2,113 @@
 import fs from 'fs';
 import inquirer from 'inquirer';
 
-// TODO: Create an array of questions for user input
-function generateREADME(answers) {
-    return `
-  # ${answers.projectTitle}
-  
-  ## Description
-  ${answers.description}
-  
-  ## Installation
-  ${answers.installation}
-  
-  ## Usage
-  ${answers.usage}
-  
-  ## License
-  This project is licensed under the [${answers.license} License](https://opensource.org/licenses/${answers.license}).
-  
-  ## Contributing
-  ${answers.contributing}
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the title of your project?'
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Provide a description of your project:'
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'Provide installation instructions:'
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'Provide usage information:'
+    },
+    {
+      type: 'input',
+      name: 'contributing',
+      message: 'Provide contribution guidelines:'
+    },
+    {
+      type: 'input',
+      name: 'tests',
+      message: 'Provide test instructions:'
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'Choose a license for your project:',
+      choices: ['MIT', 'GPL', 'Apache 2.0', 'BSD']
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your GitHub username:'
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'Enter your email address:'
+    }
+  ]);
+};
 
-  ## GitHub username
-  ${answers.github}
-  
-  ## Tests
-  ${answers.tests}
-  
-  ## Contact
-  ${answers.contact}
-    `;
-  }
+// Function to generate the README content
+const generateREADME = (answers) => {
+  return `
+# ${answers.title}
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-    fs.writeFileSync(fileName, data, 'utf8');
-    console.log(`${fileName} has been generated successfully!`);
-  }
-  
-  // Prompt the user for input and generate the README
-  inquirer
-    .prompt([
-      {
-        type: 'input',
-        name: 'projectTitle',
-        message: 'What is the title of your project?',
-      },
-      {
-        type: 'input',
-        name: 'description',
-        message: 'Provide a short description of your project:',
-      },
-      {
-        type: 'input',
-        name: 'installation',
-        message: 'What are the installation instructions?',
-      },
-      {
-        type: 'input',
-        name: 'usage',
-        message: 'How do you use this project?',
-      },
-      {
-        type: 'list',
-        name: 'license',
-        message: 'What license does your project use?',
-        choices: ['MIT', 'GPL', 'Apache', 'None'],
-      },
-      {
-        type: 'input',
-        name: 'contributing',
-        message: 'What are the contributing guidelines?',
-      },
-      {
-        type: 'input',
-        name: 'github',
-        message: 'Please enter github username?',
-      },
-      {
-        type: 'input',
-        name: 'tests',
-        message: 'How can someone test your project?',
-      },
-      {
-        type: 'input',
-        name: 'contact',
-        message: 'What is your contact information?',
-      },
-    ])
+![License Badge](https://img.shields.io/badge/license-${answers.license}-blue)
+
+## Description
+${answers.description}
+
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+${answers.installation}
+
+## Usage
+${answers.usage}
+
+## License
+This project is licensed under the ${answers.license} License.
+
+## Contributing
+${answers.contributing}
+
+## Tests
+${answers.tests}
+
+## Questions
+For any questions, please contact me via:
+- GitHub: [${answers.github}](https://github.com/${answers.github})
+- Email: ${answers.email}
+  `;
+};
+
+// Function to write the README file
+const writeToFile = (fileName, data) => {
+  fs.writeFileSync(fileName, data);
+  console.log("README.md has been generated!");
+};
+
+// Main function to run the application
+const init = () => {
+  promptUser()
     .then((answers) => {
       const readmeContent = generateREADME(answers);
       writeToFile('README.md', readmeContent);
-    });
+    })
+    .catch((err) => console.error(err));
+};
 
-
-// TODO: Create a function to initialize app
-function init() {
-}
-
-// Function call to initialize app
+// Initialize the app
 init();
